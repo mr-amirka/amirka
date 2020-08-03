fb7ac32431f2fd629134755d8924b23bff976293
Amir Absolutely <mr.amirka@ya.ru>
2020-08-03 16:51:43 +0300

    v0.0.106

--- async/interval.js
+++ async/interval.js
@@ -4,8 +4,8 @@ const delay = require('../delay');
 module.exports = (fn, _delay) => {
   let _cancel = noop;
   function frame() {
-    fn();
     _cancel = delay(frame, _delay);
+    fn();
   }
   frame();
   return () => {
